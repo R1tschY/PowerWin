b26689701c1c7a5a5887eab07bd1f1e9916e1901
R1tschY <r1tschy@yahoo.de>
2017-07-09 14:00:36 +0200

    [app] translations for modules

--- app/translations/PowerWinApp_de.ts
+++ app/translations/PowerWinApp_de.ts
@@ -4,29 +4,46 @@
 <context>
     <name>PowerWin::PowerWinApp</name>
     <message>
-        <location filename="../powerwinapp.cpp" line="106"/>
+        <location filename="../powerwinapp.cpp" line="110"/>
         <source>Start with Windows</source>
         <translation>Mit Windows starten</translation>
     </message>
     <message>
-        <location filename="../powerwinapp.cpp" line="177"/>
+        <location filename="../powerwinapp.cpp" line="181"/>
         <source>Info</source>
         <translation>&amp;Info</translation>
     </message>
     <message>
-        <location filename="../powerwinapp.cpp" line="181"/>
+        <location filename="../powerwinapp.cpp" line="185"/>
         <source>Copyright © 2017 R1tschY</source>
         <translation>Copyright © 2017 R1tschY</translation>
     </message>
     <message>
-        <location filename="../powerwinapp.cpp" line="183"/>
+        <location filename="../powerwinapp.cpp" line="187"/>
         <source>Licenced under GPL v3</source>
         <translation>Lizenziert unter GPL v3</translation>
     </message>
     <message>
-        <location filename="../powerwinapp.cpp" line="189"/>
+        <location filename="../powerwinapp.cpp" line="193"/>
         <source>Quit</source>
         <translation>&amp;Beenden</translation>
+    </message>
+</context>
+<context>
+    <name>PowerWin::WKillWindow</name>
+    <message>
+        <location filename="../../modules/wkill/wkill.cpp" line="78"/>
+        <source>Drag on Window</source>
+        <translation>Zeihe auf Fenster</translation>
+    </message>
+</context>
+<context>
+    <name>SystemMenuModule</name>
+    <message>
+        <location filename="../../modules/system-menu/systemmenu.cpp" line="51"/>
+        <location filename="../../modules/system-menu/systemmenu.cpp" line="96"/>
+        <source>Always on top</source>
+        <translation>Immer im Vordergrund</translation>
     </message>
 </context>
 </TS>
