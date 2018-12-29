d5fc05606bdc051be0e9e674a30595a2cadfdc1b
R1tschY <r1tschy@yahoo.de>
2018-12-29 16:17:07 +0100

    get_known_folder_path added

---
+++ lightports-gui/src/shell/known_folder.rs
@@ -0,0 +1,13 @@
+use winapi::um::shlobj::SHGetKnownFolderPath;
+use std::ptr;
+use lightports::{hresult, Result, FromWide};
+use std::path::PathBuf;
+use winapi::um::shtypes::KNOWNFOLDERID;
+
+pub fn get_known_folder_path(fid: *const KNOWNFOLDERID, flags: u32) -> Result<PathBuf> {
+    let mut path: *mut u16 = 0 as *mut u16;
+    hresult(unsafe { SHGetKnownFolderPath(fid, flags, ptr::null_mut(), (&mut path) as *mut *mut u16) })
+        .map(|_| {
+            PathBuf::from_wide_ptr(path)
+        })
+}
--- lightports-gui/src/shell/mod.rs
+++ lightports-gui/src/shell/mod.rs
@@ -1,3 +1,5 @@
 mod trayicon;
+mod known_folder;
 
 pub use crate::shell::trayicon::TrayIcon;
+pub use crate::shell::known_folder::get_known_folder_path;
