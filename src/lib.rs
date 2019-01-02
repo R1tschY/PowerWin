03a1d8bcea2c7750bd4353b79eb040d3971cc3a2
R1tschY <r1tschy@yahoo.de>
2019-01-02 02:09:30 +0100

    key sequence parser added

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,7 @@ extern crate lightports;
 extern crate lightports_gui;
 extern crate winapi;
 extern crate lazy_static;
+#[macro_use] extern crate nom;
 
 pub mod actions;
+pub mod key_sequence_parser;
